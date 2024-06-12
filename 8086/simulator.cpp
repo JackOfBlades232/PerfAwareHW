bab85652b32bb5e18e473781175d5d9710757738
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-06-12 14:26:16 +0300

    R/imm movs done

--- 8086/simulator.cpp
+++ 8086/simulator.cpp
@@ -4,32 +4,32 @@
 #include "instruction.hpp"
 #include <cassert>
 
-// @TODO: trace enabled/disabled
+// @TODO: think again if asserts are appropriate here.
+//        Where are we dealing with caller mistakes, and where with bad input?
 
-static u32 g_tracing_flags = 0;
+struct machine_t {
+    u16 registers[e_reg_max];
+};
 
-// @TODO: align messages
-// @TODO: four digits on final regs
+struct tracing_state_t {
+    u32 flags = 0;
+    u32 registers_used = 0;
+};
 
-// @TODO: concise machine state
-
-static u16 g_registers_state[e_reg_max];
-static u32 g_registers_used = 0;
+static machine_t g_machine = {};
+static tracing_state_t g_tracing = {};
 
 void set_simulation_trace_level(u32 flags)
 {
-    g_tracing_flags = flags;
+    g_tracing.flags = flags;
 }
-
-// @TODO: instruction execution design
-//        (anything better than just funcs for all?)
 
 static u16 read_reg(reg_access_t access)
 {
     assert((access.offset == 0 && access.size == 2) ||
            (access.reg <= e_reg_d && access.size == 1 && access.offset <= 1));
 
-    u16 reg_val = g_registers_state[access.reg];
+    u16 reg_val = g_machine.registers[access.reg];
     if (access.size == 2) // => offset = 0
         return reg_val;
     else if (access.offset == 0) // => size == 1
@@ -44,9 +44,9 @@ static void write_reg(reg_access_t access, u16 val)
            (access.reg <= e_reg_d && access.size == 1 && access.offset <= 1));
     assert(access.size == 2 || val < (1 << 8));
 
-    g_registers_used |= to_flag(access.reg);
+    g_tracing.registers_used |= to_flag(access.reg);
 
-    u16 *reg_ptr = &g_registers_state[access.reg];
+    u16 *reg_ptr = &g_machine.registers[access.reg];
     u16 prev_reg_content = *reg_ptr;
 
     if (access.size == 2) // => offset = 0
@@ -60,51 +60,62 @@ static void write_reg(reg_access_t access, u16 val)
     }
 
     // @TODO: more elegant/general side-effect tracing. No ideas for design now
-    if (g_tracing_flags & e_trace_data_mutation) {
+    if (g_tracing.flags & e_trace_data_mutation) {
         output::print(" ; ");
         output::print_word_reg(access.reg);
         output::print(":0x%hx->0x%hx\n", prev_reg_content, *reg_ptr);
     }
 }
 
-// @TODO: read/write at operand => refactor
+// @TODO: spec setting for reading EA in mem (in lea)
+static u16 read_operand(operand_t op)
+{
+    switch (op.type) {
+        case e_operand_reg:
+            return read_reg(op.data.reg);
+
+        case e_operand_imm:
+            return op.data.imm;
+
+        case e_operand_mem:
+        case e_operand_cs_ip:
+            LOGERR("Operand read not implemented for these types");
+        case e_operand_none:
+            assert(0);
+    }
+
+    return 0;
+}
+
+static void write_operand(operand_t op, u16 val)
+{
+    switch (op.type) {
+        case e_operand_reg:
+            write_reg(op.data.reg, val);
+            break;
+
+        case e_operand_imm:
+            LOGERR("Can't write to immediate operand");
+            assert(0);
+
+        case e_operand_mem:
+        case e_operand_cs_ip:
+            LOGERR("Operand read not implemented for these types");
+        case e_operand_none:
+            assert(0);
+    }
+}
 
 void simulate_instruction_execution(instruction_t instr)
 {
-    if (g_tracing_flags & e_trace_disassembly)
+    if (g_tracing.flags & e_trace_disassembly)
         output::print_intstruction(instr);
 
     // @TODO: correct instruction format validation
     switch (instr.op) {
-        case e_op_mov: {
-            u16 val;
-
-            // src
-            switch (instr.operands[1].type) {
-                case e_operand_reg:
-                    val = read_reg(instr.operands[1].data.reg);
-                    break;
-
-                case e_operand_imm:
-                    val = instr.operands[1].data.imm;
-                    break;
-
-                default: 
-                    LOGERR("Instruction execution not implemented for these operands");
-                    assert(0);
-            }
-
-            // dst
-            switch (instr.operands[0].type) {
-                case e_operand_reg:
-                    write_reg(instr.operands[0].data.reg, val);
-                    break;        
-
-                default: 
-                    LOGERR("Instruction execution not implemented for these operands");
-                    assert(0);
-            }
-        } break;
+        case e_op_mov:
+            write_operand(instr.operands[0], read_operand(instr.operands[1]));
+            break;
 
         default:
             LOGERR("Instruction execution not implemented");
@@ -114,11 +125,11 @@ void simulate_instruction_execution(instruction_t instr)
 
 void output_simulation_results()
 {
-    if (g_tracing_flags != 0)
+    if (g_tracing.flags != 0)
         output::print("\n");
     output::print("Registers state:\n");
     for (int reg = e_reg_a; reg < e_reg_ip; ++reg) {
-        if (g_registers_used & to_flag(reg)) {
+        if (g_tracing.registers_used & to_flag(reg)) {
             u16 val = read_reg(get_word_reg_access((reg_t)reg));
             output::print("      ");
             output::print_word_reg((reg_t)reg);
