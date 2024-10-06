8bc8f18e7778cf36517dc627990efa2bd65cd5ce
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-10-06 22:03:23 +0300

    Thrown some to-be-slow OOP code to start the JSON parser, not tested

--- Profiling/Solver/main.cpp
+++ Profiling/Solver/main.cpp
@@ -1,7 +1,78 @@
+#include "string.hpp"
+#include "array.hpp"
+
 #include <cstdio>
+#include <cmath>
+
+struct IJsonElement {
+    // @TODO
+};
+
+struct JsonField {
+    HpString name;
+    IJsonElement *val;
+};
+
+class JsonObject : IJsonElement {
+    DynArray<JsonField> fields;
+
+    // @TODO
+};
+
+class JsonArray : IJsonElement {
+    DynArray<IJsonElement *> m_elements;
+
+    // @TODO
+};
+
+class JsonString : IJsonElement {
+    HpString m_content;
+
+    // @TODO
+};
+
+class JsonNumber : IJsonElement {
+    long double m_val;
+
+    // @TODO
+};
+
+class JsonBool : IJsonElement {
+    bool m_val;
+
+    // @TODO
+};
+
+class JsonNull : IJsonElement {
+    // @TODO
+};
+
+// @TODO: verify
+static float reference_haversine_dist(float x0, float y0, float x1, float y1)
+{
+    constexpr float c_pi = 3.14159265359f;
+
+    auto deg2rad = [](float deg) { return deg * c_pi / 180.f; };
+    auto rad2deg = [](float rad) { return rad * 180.f / c_pi; };
+
+    auto haversine = [](float rad) { return (1.f - cosf(rad)) * 0.5f; };
+
+    float dx = deg2rad(fabsf(x0 - x1));
+    float dy = deg2rad(fabsf(y0 - y1));
+    float y0r = deg2rad(y0);
+    float y1r = deg2rad(y1);
+
+    float hav_of_diff =
+        haversine(dy) + cosf(y0r)*cosf(y1r)*haversine(dx);
+
+    float rad_of_diff = acosf(1.f - 2.f*hav_of_diff);
+    return rad2deg(rad_of_diff);
+}
 
 int main(int argc, char **argv)
 {
-    printf("Hello!\n");
+    printf("TODO: parse input json!\n");
+    printf("TODO: Calc haversine!\n");
+    printf("TODO: Make it not dogshit slow!\n");
     return 0;
 }
---
+++ Profiling/Solver/string.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <cassert>
+#include <cstdint>
+
+// @TODO: pull out to some utils, and complete features when needed
+// Idea: HpString, StString, DynString, TmpString, etc
+
+class HpString {
+    char *m_data = nullptr;
+    uint32_t m_len = 0;
+
+public:
+    HpString() = default;
+    HpString(const char *ptr, uint32_t len) : m_len(len) {
+        assert(ptr);
+        assert(strlen(ptr) == len);
+
+        m_data = new char[m_len + 1];
+        strncpy(m_data, ptr, m_len);
+    }
+    explicit HpString(const char *ptr) : HpString(ptr, strlen(ptr)) {}
+
+    HpString(const HpString &other) {
+        if (&other == this)
+            return;
+
+        CopyFrom(other);
+    }
+    HpString& operator=(const HpString &other) {
+        if (&other == this)
+            return *this;
+
+        CopyFrom(other);
+        return *this;
+    }
+
+    ~HpString() {
+        if (m_data)
+            delete[] m_data;
+    }
+
+    operator const char *() const { return m_data; }
+
+    uint32_t Length() const { return m_len; }
+    const char *Begin() const { return m_data; }
+    const char *End() const { return m_data + m_len; }
+
+private:
+    void CopyFrom(const HpString &other) {
+        m_len = other.m_len;
+
+        char *old_data = m_data;
+        if (other.m_data) {
+            m_data = new char[m_len + 1];
+            strncpy(m_data, other.m_data, m_len);
+        } else
+            m_data = nullptr;
+
+        if (old_data)
+            delete[] old_data;
+    }
+};
