40e9ace882e3e5746e0f2d4fb42750591878a155
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-02-03 22:03:11 +0100

    Repetition tester WIP

---
+++ Common/repetition.hpp
@@ -0,0 +1,157 @@
+#pragma once
+
+#include "profiling.hpp"
+
+// @TODO customizable printer instead?
+#include <cstdio>
+
+class RepetitionTester {
+    enum State {
+        e_st_pending,
+        e_st_active,
+        e_st_error
+    } m_state;
+
+    uint64_t m_target_processed_bytes;
+    uint64_t m_cpu_timer_freq;
+    uint64_t m_try_renew_min_for_ticks;
+    uint64_t m_test_start_ticks;
+
+    uint64_t m_ticks;
+    uint64_t m_bytes;
+
+    uint32_t m_open_blocks;
+    uint32_t m_closed_blocks;
+
+    bool m_print_new_minimums;
+
+    uint64_t m_test_count;
+    uint64_t m_total_ticks;
+    uint64_t m_min_ticks;
+    uint64_t m_max_ticks;
+
+    const char *m_name;
+
+#define RT_ERR(text_, ...)                           \
+    do {                                             \
+        printf("Error: " text_ "\n", ##__VA_ARGS__); \
+        m_state = e_st_error;                        \
+        return;                                      \
+    } while (0) 
+#define RT_ERRET(ret_, text_, ...)                   \
+    do {                                             \
+        printf("Error: " text_ "\n", ##__VA_ARGS__); \
+        m_state = e_st_error;                        \
+        return ret_;                                 \
+    } while (0) 
+#define RT_PRINT(text_, ...) printf(text_ "\n", ##__VA_ARGS__)
+
+public:
+    RepetitionTester(const char *name,
+                     uint64_t target_bytes,
+                     uint64_t cpu_timer_freq,
+                     float seconds_for_min_renewal,
+                     bool print_minimums)
+        : m_state{e_st_pending}
+        , m_target_processed_bytes{target_bytes}
+        , m_cpu_timer_freq{cpu_timer_freq}
+        , m_try_renew_min_for_ticks{uint64_t(seconds_for_min_renewal * cpu_timer_freq)}
+        , m_print_new_minimums{print_minimums}
+        , m_name{name} {}
+
+    void Start() {
+        if (m_state != e_st_pending)
+            RT_ERR("Start called on non-pending rep tester");
+        m_test_count = 0;
+        m_total_ticks = 0;
+        m_min_ticks = uint64_t(-1);
+        m_max_ticks = 0;
+        m_ticks = 0;
+        m_bytes = 0;
+        m_open_blocks = 0;
+        m_closed_blocks = 0;
+        m_state = e_st_active;
+        m_test_start_ticks = READ_TIMER();
+    }
+
+    bool Tick() {
+        if (m_state == e_st_pending)
+            RT_ERRET(false, "Finish called on non-active rep tester");
+        else if (m_state == e_st_error)
+            return false;
+
+        uint64_t cur_ticks = READ_TIMER();
+        if (m_open_blocks) {
+            if (m_open_blocks != m_closed_blocks)
+                RT_ERRET(false, "Not all timed blocks closed in rep tester");
+            if (m_bytes != m_target_processed_bytes)
+                RT_ERRET(false, "Bytes processed in test not equal to target bytes");
+
+            ++m_test_count;
+            m_total_ticks += m_ticks;
+            if (m_max_ticks < m_ticks)
+                m_max_ticks = m_ticks;
+            if (m_min_ticks > m_ticks) {
+                m_min_ticks = m_ticks;
+                m_test_start_ticks = cur_ticks;
+                if (m_print_new_minimums)
+                    RT_PRINT("Found new min time: %Lfs", (long double)m_min_ticks / m_cpu_timer_freq);
+            }
+        }
+
+        m_ticks = 0;
+        m_bytes = 0;
+        m_open_blocks = 0;
+        m_closed_blocks = 0;
+
+        if (cur_ticks - m_test_start_ticks > m_try_renew_min_for_ticks) {
+            FinishAndPrintResults();
+            return false;
+        }
+
+        return true;
+    }
+
+    void BeginTimeBlock() {
+        ++m_open_blocks;
+        m_ticks -= READ_TIMER();
+    }
+
+    void EndTimeBlock() {
+        ++m_closed_blocks;
+        m_ticks += READ_TIMER();
+    }
+
+    void ReportProcessedBytes(uint64_t bytes) { m_bytes += bytes; }
+
+    // @TODO format?
+    void ReportError(const char *text) { RT_ERR("%s", text); }
+
+private:
+    void FinishAndPrintResults() {
+        m_state = e_st_pending;
+
+        // @TODO: pull out these computations
+        auto gb_per_sec = [](long double sec, uint64_t bytes) {
+            constexpr long double c_bytes_in_gb = (long double)(1u << 30);
+            const long double gb = (long double)bytes / c_bytes_in_gb; 
+            return gb / sec;
+        };
+        const long double min_sec = (long double)m_min_ticks / m_cpu_timer_freq;
+        const long double max_sec = (long double)m_max_ticks / m_cpu_timer_freq;
+
+        const uint64_t avg_ticks = m_total_ticks / m_test_count;
+        const long double avg_sec = ((long double)m_total_ticks / m_test_count) / m_cpu_timer_freq;
+
+        RT_PRINT("");
+        RT_PRINT("--- %s ---", m_name);
+        RT_PRINT("Min: %Lf (%llu), %Lfgb/s", min_sec, m_min_ticks, gb_per_sec(min_sec, m_target_processed_bytes));
+        RT_PRINT("Max: %Lf (%llu), %Lfgb/s", max_sec, m_max_ticks, gb_per_sec(max_sec, m_target_processed_bytes));
+        RT_PRINT("Avg: %Lf (%llu), %Lfgb/s", avg_sec, avg_ticks, gb_per_sec(avg_sec, m_target_processed_bytes));
+        RT_PRINT("");
+    }
+
+#undef RT_ERR
+#undef RT_ERRET
+#undef RT_PRINT
+};
--- Testing/main.cpp
+++ Testing/main.cpp
@@ -1,9 +1,78 @@
+#include <repetition.hpp>
 #include <profiling.hpp>
 
 #include <cstdio>
 
-int main()
+// @TODO: pull out as generic data buffer?
+struct file_t {
+    char *data;
+    size_t len;
+
+    bool Loaded() const { return data != nullptr; }
+};
+
+static void free_file(file_t &f)
 {
-    printf("Hello, tester!\n");
+    if (f.Loaded()) {
+        delete f.data;
+        f.data = nullptr;
+        f.len = 0;
+    }
+}
+
+// @TODO: verify correctness
+
+// @TODO: sort this out (construction vs reset, Casey is onto smth)
+static uint64_t g_cpu_timer_freq = uint64_t(-1);
+
+static void fread_rep_test(const char *fn)
+{
+    // @TODO: > 4g files, aligned alloc
+    FILE *f = fopen(fn, "rb");
+    if (!f) {
+        fprintf(stderr, "Failed to open %s for fread\n", fn);
+        return;
+    }
+
+    fseek(f, 0, SEEK_END);
+    long flen = ftell(f);
+
+    file_t fmem = {new char[flen], size_t(flen)};
+    fclose(f);
+
+    RepetitionTester rt{"fread", (uint64_t)flen, g_cpu_timer_freq, 10.f, true};
+    rt.Start();
+
+    do {
+        FILE *f = fopen(fn, "rb");
+        fseek(f, 0, SEEK_SET);
+
+        rt.BeginTimeBlock();
+        size_t read = fread(fmem.data, fmem.len, 1, f);
+        rt.EndTimeBlock();
+
+        if (read != 1)
+            rt.ReportError("Failed read");
+
+        rt.ReportProcessedBytes(read * fmem.len);
+
+        fclose(f);
+    } while (rt.Tick());
+
+    free_file(fmem);
+}
+
+// @TODO: _read, ReadFile, unix syscalls
+
+int main(int argc, char **argv)
+{
+    g_cpu_timer_freq = measure_cpu_timer_freq(0.1l);
+
+    if (argc < 2) {
+        fprintf(stderr, "Invalid args, usage: prog.exe [file name]\n");
+        return 1;
+    }
+
+    fread_rep_test(argv[1]);
     return 0;
 }
