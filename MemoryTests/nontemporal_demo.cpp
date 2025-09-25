fd6bc593ca79c0b57bfa653c64d7b7e830477574
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-09-25 11:58:18 +0300

    upgrade nt demo

--- MemoryTests/nontemporal_demo.cpp
+++ MemoryTests/nontemporal_demo.cpp
@@ -9,49 +9,37 @@
 #include <cstdint>
 #include <cassert>
 
+#define RT_STOP_TIME 10.f
+
 extern "C"
 {
 extern void memcpy512(void *to, void *from, uint64_t bytecnt);
 extern void memcpy512_nt(void *to, void *from, uint64_t bytecnt);
 }
 
-template <class TCallable>
-static void run_test(
-    TCallable &&tested, uint64_t target_bytes, RepetitionTester &rt,
-    char const *name, uint64_t cpu_timer_freq, bool write_csv)
-{
-    repetition_test_results_t results{};
-
-    rt.ReStart(results, target_bytes);
-    do {
-        rt.BeginTimeBlock();
-        uint64_t bytes_cnt = tested();
-        rt.EndTimeBlock();
-        rt.ReportProcessedBytes(bytes_cnt);
-    } while (rt.Tick());
-
-    if (write_csv)
-        print_best_bandwidth_csv(results, target_bytes, cpu_timer_freq, name);
-    else
-        print_reptest_results(results, target_bytes, cpu_timer_freq, name, true);
-}
+using test_func_t = void (*)(void *, void *, uint64_t);
 
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: <program> [pattern kb] [pattern count] (csv)\n");
+    if (argc < 2) {
+        fprintf(stderr, "Usage: <program> [pattern kb] (csv)\n");
         return 1;
     }
 
+    constexpr size_t c_pattern_counts[] = { 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576 };
+    constexpr size_t c_scale_count = sizeof(c_pattern_counts) / sizeof(c_pattern_counts[0]);
+
+    constexpr test_func_t c_test_funcs[] = { &memcpy512, &memcpy512_nt };
+    constexpr size_t c_test_func_count = sizeof(c_test_funcs) / sizeof(c_test_funcs[0]);
+
+    constexpr char const *c_test_func_names[c_test_func_count] = { "Regular stores", "Nontemporal stores" };
+
     size_t const pattern_byte_count = atol(argv[1]) << 10;
-    size_t const pattern_count = atol(argv[2]);
-    size_t const output_byte_count = pattern_count * pattern_byte_count;
-    bool const write_csv = argc > 3 && streq(argv[3], "csv");
+    size_t const output_byte_count = c_pattern_counts[c_scale_count - 1] * pattern_byte_count;
+    bool const write_csv = argc > 2 && streq(argv[2], "csv");
 
     init_os_process_state(g_os_proc_state);
     uint64_t cpu_timer_freq = measure_cpu_timer_freq(0.1l);
-
-    RepetitionTester rt{output_byte_count, cpu_timer_freq, 10.f, false};
 
     char *pattern_mem = (char *)allocate_os_pages_memory(pattern_byte_count);
     char *output_mem = (char *)allocate_os_pages_memory(output_byte_count);
@@ -64,25 +52,52 @@ int main(int argc, char **argv)
     for (size_t i = 0; i < pattern_byte_count; ++i)
         pattern_mem[i] = "deadbeef"[i & 7];
 
-    if (write_csv)
-        printf("name,bytes,seconds,gb/s\n");
+    if (write_csv) {
+        printf("bytes");
+        for (char const *name : c_test_func_names)
+            printf(",%s", name);
+        printf("\n");
+    }
 
-    run_test(
-        [&] {
-            char *dst = output_mem;
-            for (size_t i = 0; i < pattern_count; ++i, dst += pattern_byte_count)
-                memcpy512(dst, pattern_mem, pattern_byte_count);
-            return output_byte_count;
-        },
-        output_byte_count, rt, "regular stores", cpu_timer_freq, write_csv);
-    run_test(
-        [&] {
-            char *dst = output_mem;
-            for (size_t i = 0; i < pattern_count; ++i, dst += pattern_byte_count)
-                memcpy512_nt(dst, pattern_mem, pattern_byte_count);
-            return output_byte_count;
-        },
-        output_byte_count, rt, "nontemporal stores", cpu_timer_freq, write_csv);
+    RepetitionTester rt{output_byte_count, cpu_timer_freq, RT_STOP_TIME, false};
+    repetition_test_results_t results{};
+
+    for (size_t count : c_pattern_counts) {
+        if (write_csv)
+            printf("%llu", pattern_byte_count * count);
+        for (size_t fi = 0; test_func_t func : c_test_funcs) {
+            rt.ReStart(results, output_byte_count);
+            do {
+                rt.BeginTimeBlock();
+                {
+                    for (size_t i = 0, end = output_byte_count / (pattern_byte_count * count); i < end; ++i) {
+                        char *dst = output_mem;
+                        for (size_t j = 0; j < count; ++j, dst += pattern_byte_count)
+                            (*func)(dst, pattern_mem, pattern_byte_count);
+                    }
+                }
+                rt.EndTimeBlock();
+                rt.ReportProcessedBytes(output_byte_count);
+            } while (rt.Tick());
+
+            if (write_csv) {
+                printf(",%Lf", gb_per_measure(
+                    (long double)results.min_ticks / cpu_timer_freq, output_byte_count));
+            } else {
+                char namebuf[256];
+                snprintf(
+                    namebuf, sizeof(namebuf), "%s (%llukb repeated over %llukb)",
+                    c_test_func_names[fi], pattern_byte_count >> 10,
+                    (count * pattern_byte_count) >> 10);
+                print_reptest_results(results, output_byte_count, cpu_timer_freq, namebuf, true);
+            }
+
+            ++fi;
+        }
+
+        if (write_csv)
+            printf("\n");
+    }
 
     free_os_pages_memory(pattern_mem, pattern_byte_count);
     free_os_pages_memory(output_mem, output_byte_count);
