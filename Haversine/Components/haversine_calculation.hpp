1ab4d00a691e6d37d16de5eea81dfed375d09501
petr.kiyashko <petr.kiyashko@gaijin.team>
2025-10-07 22:22:31 +0200

    More optimization -- vectorized cosine approx for the 4 cosines, branch eradication. Now 2.33g/s throughput vs 1.1 non inline

--- Haversine/Components/haversine_calculation.hpp
+++ Haversine/Components/haversine_calculation.hpp
@@ -97,6 +97,12 @@ inline void calculate_haversine_distances_inline(haversine_state_t &s)
     point_pair_t const *src = s.pairs;
     f64 *dst = s.answers;
 
+    // @TODO: make a proper helper func? Can't decide on return types yet.
+    auto ternary = [](bool cond, f64 l, f64 r) {
+        return _mm_cvtsd_f64(_mm_blendv_pd(
+            _mm_set_sd(r), _mm_set_sd(l), bool2mask_sd(cond)));
+    };
+
     for (u32 i = 0; i < s.pair_cnt; ++i, ++src) {
         f64 xr0 = c_deg2rad * src->x0 + c_pi_half64;
         f64 xr1 = c_deg2rad * src->x1 + c_pi_half64;
@@ -105,94 +111,66 @@ inline void calculate_haversine_distances_inline(haversine_state_t &s)
 
         f64 dxr = xr0 - xr1;
         f64 dyr = yr0 - yr1;
-        f64 adxr = dxr < 0.0 ? -dxr : dxr;
-        f64 dx = adxr > c_pi64 ? c_2pi64 - adxr : adxr;
-        f64 dy = dyr < 0.0 ? -dyr : dyr; // Already from -pi to pi
+        f64 adxr = ternary(dxr < 0.0, -dxr, dxr);
+        f64 dx = ternary(adxr > c_pi64, c_2pi64 - adxr, adxr);
+        f64 dy = ternary(dyr < 0.0, -dyr, dyr); // Already from -pi to pi
 
-        f64 cos_x0_x = xr0 > c_pi_half64 ? c_pi64 - xr0 : xr0;
-        f64 cos_x0_x2 = cos_x0_x * cos_x0_x;
-        f64 cos_x0 = 0x1.883c1c5deffbep-49;
-        cos_x0 = fma(cos_x0, cos_x0_x2, -0x1.ae43dc9bf8ba7p-41);
-        cos_x0 = fma(cos_x0, cos_x0_x2, 0x1.6123ce513b09fp-33);
-        cos_x0 = fma(cos_x0, cos_x0_x2, -0x1.ae6454d960ac4p-26);
-        cos_x0 = fma(cos_x0, cos_x0_x2, 0x1.71de3a52aab96p-19);
-        cos_x0 = fma(cos_x0, cos_x0_x2, -0x1.a01a01a014eb6p-13);
-        cos_x0 = fma(cos_x0, cos_x0_x2, 0x1.11111111110c9p-7);
-        cos_x0 = fma(cos_x0, cos_x0_x2, -0x1.5555555555555p-3);
-        cos_x0 = fma(cos_x0, cos_x0_x2, 0x1p0);
-        cos_x0 *= cos_x0_x;
+        __m256d a = _mm256_set_pd(
+            c_pi_half64 - dy,
+            c_pi_half64 - dx,
+            ternary(xr1 > c_pi_half64, c_pi64 - xr1, xr1),
+            ternary(xr0 > c_pi_half64, c_pi64 - xr0, xr0));
+        __m256d a2 = _mm256_mul_pd(a, a);
+        __m256d cosines = _mm256_set1_pd(0x1.883c1c5deffbep-49);
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(-0x1.ae43dc9bf8ba7p-41));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(0x1.6123ce513b09fp-33));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(-0x1.ae6454d960ac4p-26));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(0x1.71de3a52aab96p-19));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(-0x1.a01a01a014eb6p-13));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(0x1.11111111110c9p-7));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(-0x1.5555555555555p-3));
+        cosines = _mm256_fmadd_pd(cosines, a2, _mm256_set1_pd(0x1p0));
+        cosines = _mm256_mul_pd(cosines, a);
 
-        f64 cos_x1_x = xr1 > c_pi_half64 ? c_pi64 - xr1 : xr1;
-        f64 cos_x1_x2 = cos_x1_x * cos_x1_x;
-        f64 cos_x1 = 0x1.883c1c5deffbep-49;
-        cos_x1 = fma(cos_x1, cos_x1_x2, -0x1.ae43dc9bf8ba7p-41);
-        cos_x1 = fma(cos_x1, cos_x1_x2, 0x1.6123ce513b09fp-33);
-        cos_x1 = fma(cos_x1, cos_x1_x2, -0x1.ae6454d960ac4p-26);
-        cos_x1 = fma(cos_x1, cos_x1_x2, 0x1.71de3a52aab96p-19);
-        cos_x1 = fma(cos_x1, cos_x1_x2, -0x1.a01a01a014eb6p-13);
-        cos_x1 = fma(cos_x1, cos_x1_x2, 0x1.11111111110c9p-7);
-        cos_x1 = fma(cos_x1, cos_x1_x2, -0x1.5555555555555p-3);
-        cos_x1 = fma(cos_x1, cos_x1_x2, 0x1p0);
-        cos_x1 *= cos_x1_x;
+        f64 cosx0 = _mm256_cvtsd_f64(cosines);
+        f64 cosx1 = _mm256_cvtsd_f64(_mm256_permute4x64_pd(cosines, 0b01010101));
+        f64 cosdx = _mm256_cvtsd_f64(_mm256_permute4x64_pd(cosines, 0b10101010));
+        f64 cosdy = _mm256_cvtsd_f64(_mm256_permute4x64_pd(cosines, 0b11111111));
 
-        f64 dxa = c_pi_half64 - dx;
-        f64 dxa2 = dxa * dxa;
-        f64 cos_dx = 0x1.883c1c5deffbep-49;
-        cos_dx = fma(cos_dx, dxa2, -0x1.ae43dc9bf8ba7p-41);
-        cos_dx = fma(cos_dx, dxa2, 0x1.6123ce513b09fp-33);
-        cos_dx = fma(cos_dx, dxa2, -0x1.ae6454d960ac4p-26);
-        cos_dx = fma(cos_dx, dxa2, 0x1.71de3a52aab96p-19);
-        cos_dx = fma(cos_dx, dxa2, -0x1.a01a01a014eb6p-13);
-        cos_dx = fma(cos_dx, dxa2, 0x1.11111111110c9p-7);
-        cos_dx = fma(cos_dx, dxa2, -0x1.5555555555555p-3);
-        cos_dx = fma(cos_dx, dxa2, 0x1p0);
-        cos_dx *= dxa;
-
-        f64 dya = c_pi_half64 - dy;
-        f64 dya2 = dya * dya;
-        f64 cos_dy = 0x1.883c1c5deffbep-49;
-        cos_dy = fma(cos_dy, dya2, -0x1.ae43dc9bf8ba7p-41);
-        cos_dy = fma(cos_dy, dya2, 0x1.6123ce513b09fp-33);
-        cos_dy = fma(cos_dy, dya2, -0x1.ae6454d960ac4p-26);
-        cos_dy = fma(cos_dy, dya2, 0x1.71de3a52aab96p-19);
-        cos_dy = fma(cos_dy, dya2, -0x1.a01a01a014eb6p-13);
-        cos_dy = fma(cos_dy, dya2, 0x1.11111111110c9p-7);
-        cos_dy = fma(cos_dy, dya2, -0x1.5555555555555p-3);
-        cos_dy = fma(cos_dy, dya2, 0x1p0);
-        cos_dy *= dya;
-
-        f64 cosx0_cosx1 = cos_x0 * cos_x1;
-        f64 m1_cosdy = 1.0 - cos_dy;
+        f64 cosx0_cosx1 = cosx0 * cosx1;
+        f64 m1_cosdy = 1.0 - cosdy;
         f64 mterm = cosx0_cosx1 * m1_cosdy;
+        f64 sterm = 1.0 - cosdx + mterm;
 
-        f64 sterm = 1.0 - cos_dx + mterm;
         f64 hsterm = sterm * 0.5;
 
-        bool angle_flip = hsterm > 0.5;
-        f64 angle_x2 = angle_flip ? 1.0 - hsterm : hsterm;
-        f64 angle_x = sqrt_e(angle_x2);
-        f64 angle_r = 0x1.7f820d52c2775p-1;
-        angle_r = fma(angle_r, angle_x2, -0x1.4d84801ff1aa1p1);
-        angle_r = fma(angle_r, angle_x2, 0x1.14672d35db97ep2);
-        angle_r = fma(angle_r, angle_x2, -0x1.188f223fe5f34p2);
-        angle_r = fma(angle_r, angle_x2, 0x1.86bbff2a6c7b6p1);
-        angle_r = fma(angle_r, angle_x2, -0x1.83633c76e4551p0);
-        angle_r = fma(angle_r, angle_x2, 0x1.224c4dbe13cbdp-1);
-        angle_r = fma(angle_r, angle_x2, -0x1.2ab04ba9012e3p-3);
-        angle_r = fma(angle_r, angle_x2, 0x1.5565a3d3908b9p-5);
-        angle_r = fma(angle_r, angle_x2, 0x1.b1b8d27cd7e72p-8);
-        angle_r = fma(angle_r, angle_x2, 0x1.dc086c5d99cdcp-7);
-        angle_r = fma(angle_r, angle_x2, 0x1.1b8cc838ee86ep-6);
-        angle_r = fma(angle_r, angle_x2, 0x1.6e96be6dbe49ep-6);
-        angle_r = fma(angle_r, angle_x2, 0x1.f1c6b0ea300d7p-6);
-        angle_r = fma(angle_r, angle_x2, 0x1.6db6dca9f82d4p-5);
-        angle_r = fma(angle_r, angle_x2, 0x1.3333333148aa7p-4);
-        angle_r = fma(angle_r, angle_x2, 0x1.555555555683fp-3);
-        angle_r = fma(angle_r, angle_x2, 0x1.fffffffffffffp-1);
-        angle_r *= angle_x;
-        f64 angle = angle_flip ? c_pi_half64 - angle_r : angle_r;
+        __m128d cvt_mask = bool2mask_sd(hsterm > 0.5);
+        __m128d angle_x2 = _mm_blendv_pd(
+            _mm_set_sd(hsterm), _mm_set_sd(1.0 - hsterm), cvt_mask);
+        __m128d angle_x = _mm_sqrt_sd(angle_x2, angle_x2);
+        __m128d angle_r = _mm_set_sd(0x1.7f820d52c2775p-1);
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(-0x1.4d84801ff1aa1p1));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.14672d35db97ep2));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(-0x1.188f223fe5f34p2));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.86bbff2a6c7b6p1));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(-0x1.83633c76e4551p0));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.224c4dbe13cbdp-1));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(-0x1.2ab04ba9012e3p-3));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.5565a3d3908b9p-5));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.b1b8d27cd7e72p-8));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.dc086c5d99cdcp-7));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.1b8cc838ee86ep-6));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.6e96be6dbe49ep-6));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.f1c6b0ea300d7p-6));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.6db6dca9f82d4p-5));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.3333333148aa7p-4));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.555555555683fp-3));
+        angle_r = _mm_fmadd_sd(angle_r, angle_x2, _mm_set_sd(0x1.fffffffffffffp-1));
+        angle_r = _mm_mul_sd(angle_r, angle_x);
+        __m128d angle = _mm_blendv_pd(
+            angle_r, _mm_sub_sd(_mm_set_sd(c_pi_half64), angle_r), cvt_mask);
 
-        f64 dist = c_rad_coeff * angle;
+        f64 dist = c_rad_coeff * _mm_cvtsd_f64(angle);
 
         *dst++ = dist;
         s.sum_answer += dist;
